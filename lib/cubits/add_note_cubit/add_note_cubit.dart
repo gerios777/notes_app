12a9c5a03db9cf56be28b4e22b8014f81cacb860
Gerios <georgetarabain2017@gmail.com>
2026-04-10 19:22:20 +0300

    add note button sheet done

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,6 +1,6 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'package:meta/meta.dart';
 import 'package:note_app/conestant.dart';
 import 'package:note_app/models/note_model.dart';
 
@@ -8,8 +8,9 @@ part 'add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
-
+  Color? color = Colors.blue;
   addNote(NoteModel note) async {
+    note.color = color!.value;
     emit(AddNoteLoading());
     try {
       var notesBox = Hive.box<NoteModel>(kNotesBox);
